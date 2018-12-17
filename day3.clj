4e2a109de6bbbc8b40817ca8eb7ed04ea3e58e3c
Anton Rogov <anton@blackbits.pro>
2018-12-17 13:07:11 +0300

    reverting back and finishing day 3

--- day3.clj
+++ day3.clj
@@ -1,28 +1,43 @@
-(require '[clojure.string :as str])
+(require '[clojure.test :as test])
 
 (defn claiming-points [claim]
   (let [left (:left claim)
         top (:top claim)]
-    (flatten
+    (apply concat
       (for [dx (range 0 (:width claim))]
         (for [dy (range 0 (:height claim))]
-          {:id (:id claim) :point [(+ left dx) (+ top dy)]})))))
+          [(+ left dx) (+ top dy)])))))
 
-(defn claim-points [claimed {:keys [id point]}]
-  (assoc claimed point
-         (if-let [old-ids (get claimed point)]
-           (conj old-ids id)
-           (vector id))))
+(defn claim-point [claimed point]
+  (let [old-value (get claimed point)
+        new-value (if old-value (inc old-value) 1)]
+    (assoc claimed point new-value)))
+
+(defn claim-all-points [claims]
+  (reduce claim-point {} (apply concat (map claiming-points claims))))
 
 (defn count-overlapping-points [claims]
   (count
-    (filter (fn [[point ids]] (> (count ids) 1))
-      (reduce claim-points {} (flatten (map claiming-points claims))))))
+    (filter (fn [[point n]] (> n 1))
+      (claim-all-points claims))))
 
-(defn non-overlapping-claim-ids [claims]
-  ; (map second
-    (filter (fn [[point ids]] (> (count ids) 1))
-      (reduce claim-points {} (flatten (map claiming-points claims)))));)
+(test/is (= (count-overlapping-points [{:id 1 :left 1 :top 3 :width 4 :height 4 }
+                                       {:id 2 :left 3 :top 1 :width 4 :height 4 }
+                                       {:id 3 :left 5 :top 5 :width 2 :height 2 }]) 4))
+
+
+(defn first-non-overlapping-claim-id [claims]
+  (let [claimed-points (claim-all-points claims)]
+    (some (fn [claim]
+            (if (every? #(= (get claimed-points %) 1)
+                        (claiming-points claim))
+              (:id claim)))
+          claims)))
+
+(test/is
+      (= (first-non-overlapping-claim-id [{:id 1 :left 1 :top 3 :width 4 :height 4 }
+                                          {:id 2 :left 3 :top 1 :width 4 :height 4 }
+                                          {:id 3 :left 5 :top 5 :width 2 :height 2 }]) 3))
 
 (defn parse-claim [s]
   (let [[id left top width height]
@@ -30,19 +45,8 @@
              (rest (re-matches #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)" s)))]
     {:id id :left left :top top :width width :height height}))
 
-; (prn (claiming-points {:id 1 :left 1 :top 3 :width 4 :height 4 }))
-; (prn (reduce claim-points {} (claiming-points {:id 1 :left 1 :top 3 :width 4 :height 4 })))
-(prn (= (count-overlapping-points [{:id 1 :left 1 :top 3 :width 4 :height 4 }
-                                   {:id 2 :left 3 :top 1 :width 4 :height 4 }
-                                   {:id 3 :left 5 :top 5 :width 2 :height 2 }]) 4))
-(prn (= (parse-claim "#1 @ 258,327: 19x22")
-        {:id 1 :left 258 :top 327 :width 19 :height 22}))
-(prn (non-overlapping-claim-ids [{:id 1 :left 1 :top 3 :width 4 :height 4 }
-                                    {:id 2 :left 3 :top 1 :width 4 :height 4 }
-                                    {:id 3 :left 5 :top 5 :width 2 :height 2 }]))
-(prn (= (non-overlapping-claim-ids [{:id 1 :left 1 :top 3 :width 4 :height 4 }
-                                    {:id 2 :left 3 :top 1 :width 4 :height 4 }
-                                    {:id 3 :left 5 :top 5 :width 2 :height 2 }]) [3]))
+(test/is (= (parse-claim "#1 @ 258,327: 19x22")
+            {:id 1 :left 258 :top 327 :width 19 :height 22}))
 
-; (with-open [rdr (clojure.java.io/reader "day3.txt")]
-;   (println (count-overlapping-points (map parse-claim (line-seq rdr)))))
+(with-open [rdr (clojure.java.io/reader "day3.txt")]
+  (println (first-non-overlapping-claim-id (map parse-claim (line-seq rdr)))))
